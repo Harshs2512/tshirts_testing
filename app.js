e73657458d580795ac741c423e90bc77b68fdfa3
Harsh Soni <108936128+Harshs2512@users.noreply.github.com>
2023-07-04 15:22:13 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 require("dotenv").config();
-require("./db");
+require("db");
 const userRoute = require("./routes/userRoute");
 const app = express();
 const cookieParser = require("cookie-parser");
