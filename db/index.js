a95b44f4618add961c51a0e7d787edc8bea0b05f
Harsh Soni <108936128+Harshs2512@users.noreply.github.com>
2023-07-04 17:52:16 +0530

    Update index.js

--- db/index.js
+++ db/index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
-
+const DB_URI = "mongodb+srv://harshsoni123:ucM3p2K7NiugYNoW@tshirthub.hha3dev.mongodb.net/?retryWrites=true&w=majority"
 mongoose
-.connect('mongodb+srv://harshsoni123:ucM3p2K7NiugYNoW@tshirthub.hha3dev.mongodb.net/?retryWrites=true&w=majority')
+.connect(DB_URI)
 .then(() => {
 console.log('db is connected!')
 })
